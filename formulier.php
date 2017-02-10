12c2cb10c778dc284e5f3fdfe77ac6b9e3afb21c
thymen heersmink <jt.heersmink@student.alfa-college.nl>
2017-02-10 16:49:17 +0100

    php

---
+++ formulier.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="nl">
+	<head>
+		<meta http-equiv="Content-Type"
+		content="text/html;
+		charset=UTF-8" />
+	</head>
+<body>
+	<form action="verwerken.php" method="post">
+	<input type="text" name="naam"><br>
+	<input type="text" name="kleur"><br>
+
+	<input type="submit" name="submit" value="Versturen">
+	
+</body>
+</html>
---
+++ verwerken.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+	<head>
+		<meta http-equiv="Content-Type"
+		content="text/html;
+		charset=UTF-8" />
+	</head>
+	<body>
+	
+<?php
+if(isset($_POST["submit"]) ){
+
+echo $_POST["naam"]. "<BR>";
+echo $_POST["kleur"]. "<BR>";
+?>
+
+</body>
+</html>
